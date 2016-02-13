c25c4bc8bf372b06b540e72a0788902d54d1a117
valentinchevalier <valche.vc@gmail.com>
2016-02-13 14:42:00 +0100

    Mise en place de la première version du workspace

--- js/controllers/WorkspaceCtrl.js
+++ js/controllers/WorkspaceCtrl.js
@@ -1,3 +1,25 @@
 app.controller("WorkspaceCtrl", function($scope){
     $scope.nom = "Les 4 Développeurs";
+    $scope.isMenuVisible = false;
+    $scope.buttonIcon = "fa-plus";
+    $scope.availableWidgets = [
+        {
+            title:"Widget 1",
+        },
+        {
+            title:"Widget 2",
+        },
+        {
+            title:"Widget 3",
+        },
+    ];
+
+    $scope.toggleMenu = function(){
+        $scope.isMenuVisible = !$scope.isMenuVisible;
+        if ($scope.isMenuVisible){
+            $scope.buttonIcon = "fa-arrow-down";
+        } else {
+            $scope.buttonIcon = "fa-plus";
+        }
+    }
 });
