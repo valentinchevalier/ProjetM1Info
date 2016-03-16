b2a9db15445db4f1b0bc0aecd4fdb810bd1d5fe1
Clément <clement.chaumel@gmail.com>
2016-03-16 21:29:47 +0100

    Création du widget post-it

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -40,6 +40,12 @@ app.controller("MainCtrl", function ($scope, $rootScope, UserService, SettingsSe
             deletion: false,
         },
         {
+            name: "Post It",
+            type: "post_it",
+            color: "#FFE100",
+            deletion: false,
+        },
+        {
             name: "Météo",
             color: "#33cc99",
             img_url: null,
