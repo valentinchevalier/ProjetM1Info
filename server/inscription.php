cb66c34e4f88812aa4ca345d383ce86fc025bfd7
valentinchevalier <valche.vc@gmail.com>
2016-03-18 15:24:12 +0100

    Problème de chemin de l'include

--- server/inscription.php
+++ server/inscription.php
@@ -1,5 +1,5 @@
 <?php
-require_once('server/db_config.php');
+require_once('./db_config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
     $_POST = json_decode(file_get_contents('php://input'), true);
